f5ecd115988f2e98e13b9c1d3832d14722b1efe0
JaeseongPark <sysh5656@naver.com>
2019-11-18 15:06:44 +0900

    Apply vue router

--- todo-front/src/main.js
+++ todo-front/src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+// 파일을 등록하지 않으면 index.js 부터 접근한다.
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
